0b2004c5b6b635d7ec0bf8677a841938620fb846
“Akpughe” <davidakpughe2@gmail.comgit config --global user.email davidakpughe2@gmail.com>
2021-09-17 12:17:58 +0100

    send link for reset

--- components/Forgot.js
+++ components/Forgot.js
@@ -2,14 +2,54 @@ import React from 'react';
 import styles from '../styles/Home.module.css';
 import istyles from '../styles/Input.module.css';
 import { Input, Form, Button } from 'semantic-ui-react';
-
+import { useState, useContext } from 'react';
+import axios from 'axios';
+import { useRouter } from 'next/router';
+import { AuthContext } from '../utils/authContext';
 const Forgot = ({ openS }) => {
+  const { user } = useContext(AuthContext);
+  const [formData, setFormData] = useState({
+    email: '',
+  });
+  console.log(user);
+
+  const { email } = formData;
+
+  const router = useRouter();
+
+  const onChange = (e) =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .post(`${process.env.NEXT_PUBLIC_BASE_URL}/user/reset`, {
+        ...formData,
+      })
+      .then((res) => {
+        res.data;
+        // console.log(res.data);
+        // alert('Done');
+        return router.push(`/`);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
   return (
     <div className={styles.wrap}>
       <div className={styles.button_wrap}>
-        <Form>
+        <Form onSubmit={(e) => handleSubmit(e)}>
           <Form.Field className={istyles.ffield}>
-            <input className={istyles.input} placeholder="Email" />
+            <input
+              name="email"
+              type="email"
+              value={email}
+              onChange={(e) => onChange(e)}
+              className={istyles.input}
+              placeholder="Email"
+              required
+            />
           </Form.Field>
           <Button
             type="submit"
