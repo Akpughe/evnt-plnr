d57285cf2f3f91bd1bc7d1d8cf2475e28c27075f
“Akpughe” <davidakpughe2@gmail.comgit config --global user.email davidakpughe2@gmail.com>
2021-09-18 00:39:52 +0100

    added styles 2.0

--- pages/home.js
+++ pages/home.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Navbar from '../components/Navbar';
 import { AuthContext } from '../utils/authContext';
 import { useContext } from 'react';
-import Link from 'next/link'
+import Link from 'next/link';
 import { Icon } from 'semantic-ui-react';
 import styles from '../styles/Home.module.css';
 import istyles from '../styles/Input.module.css';
@@ -14,7 +14,13 @@ const home = () => {
       <Navbar />
       <div className={styles.landing}></div>
 
-      <div style={{ display: 'flex', justifyContent: 'center', marginTop:'100px' }}>
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          marginTop: '100px',
+        }}
+      >
         <div className={istyles.bg_update1}>
           <div className={styles.update_wrap1}>
             <iframe
@@ -35,7 +41,9 @@ const home = () => {
           </div>
         </div>
         <div style={{ marginLeft: '20px' }} className={istyles.bg_update1}>
-          <div style={{ display: 'flex', flexDirection: 'column', color:'white' }}>
+          <div
+            style={{ display: 'flex', flexDirection: 'column', color: 'white' }}
+          >
             <p>
               <Icon inverted name="instagram" /> Intelligent Crazy People
             </p>
@@ -46,15 +54,15 @@ const home = () => {
               <Icon inverted name="facebook f" /> groups/intelligentcrazypeople
             </p>
             <p>
-              <Icon inverted name="discord d" /> discord.gg/3Mkeu5
+              <Icon inverted name="discord" /> discord.gg/3Mkeu5
             </p>
             <p>
               <Icon inverted name="instagram" /> Instagram
             </p>
             <Link href="/faq">
-            <p>
-              <Icon inverted name="info circle" /> Privacy . Contact
-            </p>
+              <p style={{ cursor: 'pointer' }}>
+                <Icon inverted name="info circle" /> Privacy . Contact
+              </p>
             </Link>
           </div>
         </div>
